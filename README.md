1459cab6251d7bdd4de8397761ddb269646b9cd6
Fatescript <422643055@qq.com>
2016-04-19 19:27:09 +0800

    finish chapter4

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ It contains all codes when I was learning ***Programming Collective Intelligence
 
 #Instruction  
 
-点击下载，使用python运行即可  
-Download them and run them with python
+点击下载，使用python运行即可（ipython更好）  
+Download them and run them with python(ipython better)
 # Language  
 
   * Python
@@ -21,7 +21,11 @@ Download them and run them with python
 ### 2016.4.14
   * 完成并且上传第三章代码  
    complete and upload the source code of 3rd chapter
- 
+
+### 2016.4.19
+  * 完成并且上传第四章代码  
+   complete and upload the source code of 4th chapter 
+
 # About  
 
 * author : [Fatescript](https://github.com/FateScript)
--- chapter4/nn.py
+++ chapter4/nn.py
@@ -1,6 +1,9 @@
 from math import tanh
 from sqlite3 import dbapi2 as sqlite
 
+def dtanh(y):
+    return 1.0-y*y
+
 class searchnet:
     
     def __init__(self,dbname):
@@ -13,7 +16,7 @@ class searchnet:
         self.con.execute('create table hiddennode(create_key)')
         self.con.execute('create table wordhidden(fromid,toid,strength)')
         self.con.execute('create table hiddenurl(fromid,toid,strength)')
-        self.con.dbcommit()
+        self.con.commit()
         
     def getstrength(self,fromid,toid,layer):
         if layer == 0:
@@ -21,7 +24,8 @@ class searchnet:
         else:
             table = 'hiddenurl'
         res = self.con.execute(
-        'select strength from %s where fromid=%d and toid=%d'%(table,fromid,toid)).fetchone()
+        'select strength from %s where fromid=%d and toid=%d'%(table,fromid,toid)
+        ).fetchone()
         if res == None:
             if layer == 0:
                 return -0.2
@@ -39,10 +43,12 @@ class searchnet:
         'select rowid from %s where fromid=%d and toid=%d'%(table,fromid,toid)).fetchone()
         if res==None:
             self.con.execute(
-            'insert into %s(fromid,toid,strength) values (%d,%d,%f)'%(table,fromid,toid,strength))
+            'insert into %s(fromid,toid,strength) values (%d,%d,%f)'
+            %(table,fromid,toid,strength))
         else :
             rowid = res[0]
-            self.con.execute('update %s set strength=%f where rowid=%d'%(table,strength,rowid))
+            self.con.execute('update %s set strength=%f where rowid=%d'
+            %(table,strength,rowid))
         
         
     def generatehiddennode(self,wordids,urls):
@@ -63,13 +69,139 @@ class searchnet:
                 self.setstrength(hiddenid,urlid,1,0.1)
             self.con.commit()
             
+    def getallhiddenids(self,wordids,urlids):
+        l1 = {}
+        for wordid in wordids:
+            cur = self.con.execute(
+            'select toid from wordhidden where fromid=%d'%wordid)
+            for row in cur:
+                l1[row[0]] = 1
+        for urlid in urlids:
+            cur = self.con.execute(
+            'select fromid from hiddenurl where toid=%d' %urlid)
+            for row in cur:
+                l1[row[0]] = 1
+        return l1.keys()
+        
+    
+    def setupnetwork(self,wordids,urlids):
+        self.wordids = wordids
+        self.hiddenids = self.getallhiddenids(wordids,urlids)
+        self.urlids = urlids
+        
+        self.ai = [1.0]*len(self.wordids)
+        self.ah = [1.0]*len(self.hiddenids)
+        self.ao = [1.0]*len(self.urlids)
+        
+        self.wi = [[self.getstrength(wordid,hiddenid,0) 
+        for hiddenid in self.hiddenids]
+        for wordid in self.wordids]
             
-mynet = searchnet('nn.db')
+        self.wo = [[self.getstrength(hiddenid,urlid,1)
+        for urlid in self.urlids]
+        for hiddenid in self.hiddenids]
+            
+        
+    def feedforword(self):
+        for i in range(len(self.wordids)):
+            self.ai[i] = 1.0
+        
+        for j in range(len(self.hiddenids)):
+            sum = 0.0
+            for i in range(len(self.wordids)):
+                sum += self.ai[i]*self.wi[i][j]
+            self.ah[j] = tanh(sum)
+            
+        for k in range(len(self.urlids)):
+            sum = 0.0
+            for j in range(len(self.hiddenids)):
+                sum += self.ah[j]*self.wo[j][k]
+            self.ao[k] = tanh(sum)
+        
+        return self.ao[:]
+        
+    
+    def getresult(self,wordids,urlids):
+        self.setupnetwork(wordids,urlids)
+        return self.feedforword()
+        
+    def backPropagate(self,targets,N=0.5):
+        output_deltas = [0.0]*len(self.urlids)
+        for k in range(len(self.urlids)):
+            error = targets[k] - self.ao[k]
+            output_deltas[k] = dtanh(self.ao[k]) * error
+            
+        hidden_deltas = [0.0]*len(self.hiddenids)
+        for j in range(len(self.hiddenids)):
+            error = 0.0
+            for k in range(len(self.urlids)):
+                error += output_deltas[k]*self.wo[j][k]
+            hidden_deltas[j] = dtanh(self.ah[j])*error
+        
+        for j in range(len(self.hiddenids)):
+            for k in range(len(self.urlids)):
+                change = output_deltas[k]*self.ah[j]
+                self.wo[j][k] += N*change
+                
+        for i in range(len(self.wordids)):
+            for j in range(len(self.hiddenids)):
+                change = hidden_deltas[j]*self.ai[i]
+                self.wi[i][j] += N*change
+                
+    
+    def updatedatabase(self):
+        for i in range(len(self.wordids)):
+            for j in range(len(self.hiddenids)):
+                self.setstrength(self.wordids[i],self.hiddenids[j],0,self.wi[i][j])
+        
+        for j in range(len(self.hiddenids)):
+            for k in range(len(self.urlids)):
+                self.setstrength(self.hiddenids[j],self.urlids[k],1,self.wo[j][k])
+                
+        self.con.commit()
+                
+    def trainquery(self,wordids,urlids,selectedurl):
+        self.generatehiddennode(wordids,urlids)
+        
+        self.setupnetwork(wordids,urlids)
+        self.feedforword()
+        targets = [0.0]*len(urlids)
+        targets[urlids.index(selectedurl)]=1.0
+        self.backPropagate(targets)
+        self.updatedatabase()
+                
+        
+
+#wWorld,wRiver,wBank = 101,102,103
+#uWorldBank,uRiver,uEarth = 201,202,203
+#mynet = searchnet('nn.db')        
+ 
+       
+"""       
 mynet.maketables()
-wWorld,wRiver,wEarth = 101,102,103
-uWorldBank,uRiver,uEarth = 201,202,203
 mynet.generatehiddennode([wWorld,wBank],[uWorldBank,uRiver,uEarth])
 for c in mynet.con.execute('select * from wordhidden'):
     print c
 for c in mynet.con.execute('select * from hiddenurl'):
     print c
+"""        
+
+
+#print mynet.getresult([wWorld,wBank],[uWorldBank,uRiver,uEarth])
+    
+
+#mynet.trainquery([wWorld,wBank],[uWorldBank,uRiver,uEarth],uWorldBank)
+#print mynet.getresult([wWorld,wBank],[uWorldBank,uRiver,uEarth])
+
+"""
+allurls = [uWorldBank,uRiver,uEarth]
+for i in range(30):
+    mynet.trainquery([wWorld,wBank],allurls,uWorldBank)
+    mynet.trainquery([wRiver,wBank],allurls,uRiver)
+    mynet.trainquery([wWorld],allurls,uEarth)
+    
+print mynet.getresult([wWorld,wBank],allurls)
+print mynet.getresult([wRiver,wBank],allurls)
+print mynet.getresult([wBank],allurls)
+"""
+
